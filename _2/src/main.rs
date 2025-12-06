339c1a94e9b26d90aa5c0573c58df42efb1f5c6a
Seva Alipov <seva.alipov@mail.utoronto.ca>
2025-12-05 20:07:57 -0500

    Day 2 done

--- _2/src/main.rs
+++ _2/src/main.rs
@@ -1,96 +1,29 @@
 use std::fs;
 
-pub fn repeats(num: usize) -> bool { //PART 2
-    let mut num_cpy = num;
-    let subnum: usize = num_cpy % 10;
-    let mut single_repeated = true;
-    let mut repeats = 0;
-    while num_cpy > 0 {
-        repeats += 1;
-        if num_cpy % 10 != subnum {
-            single_repeated = false;
-            break;
-        }
-        num_cpy /= 10;
-    }
-    if num < 10usize.pow(repeats-1) {single_repeated = false;}
-    if repeats < 2 {single_repeated = false;}
-
-    if single_repeated {return true;}
-
-    num_cpy = num;
-    let subnum = num_cpy % 100;
-    let mut double_repeated = true;
-    repeats = 0;
-    while num_cpy > 0 {
-        repeats += 1;
-        if num_cpy % 100 != subnum {
-            double_repeated = false;
-            break;
-        }
-        num_cpy /= 100;
-    }
-    if num < 100usize.pow(repeats-1) {double_repeated = false;}
-    if repeats < 2 {double_repeated = false;}
-
-    if double_repeated {return true;}
-
-    num_cpy = num;
-    let subnum = num_cpy % 1000;
-    let mut triple_repeated = true;
-    repeats = 0;
-    while num_cpy > 0 {
-        repeats += 1;
-        if num_cpy % 1000 != subnum {
-            triple_repeated = false;
-            break;
-        }
-        num_cpy /= 1000;
-    }
-    if num < 1000usize.pow(repeats-1) {triple_repeated = false};
-    if repeats < 2 {double_repeated = false;}
-
-    if triple_repeated {return true;}
-
-    num_cpy = num;
-    let subnum = num_cpy % 10000;
-    let mut quad_repeated = true;
-    repeats = 0;
-    while num_cpy > 0 {
-        repeats += 1;
-        if num_cpy % 10000 != subnum {
-            quad_repeated = false;
-            break;
-        }
-        num_cpy /= 10000;
-    }
-    if num < 10000usize.pow(repeats-1) {quad_repeated = false};
-    if repeats < 2 {quad_repeated = false;}
-
-    if quad_repeated {return true;}
-
-    num_cpy = num;
-    let subnum = num_cpy % 100000;
-    let mut pent_repeated = true;
-    repeats = 0;
-    while num_cpy > 0 {
-        repeats += 1;
-        if num_cpy % 100000 != subnum {
-            pent_repeated = false;
-            break;
-        }
-        num_cpy /= 100000;
-    }
-    if num < 100000usize.pow(repeats-1) {pent_repeated = false;}
-    if repeats < 2 {pent_repeated = false;}
-
-    pent_repeated
+pub fn repeats_twice(str: String) -> bool { //PART 1
+    str[0..str.len()/2] == str[str.len()/2..str.len()]
 }
 
-pub fn repeats_twice(str: String) -> bool { //PART 1
-    if str.len() % 2 != 0 {return false;}
+pub fn repeats(str: String) -> bool { //PART 2
+    let len = str.len();
+    let mut repeats: [bool; 7] = [true; 7]; //May need to expand this if your puzzles has longer numbers.
 
-    str[0..str.len()/2] == str[str.len()/2..str.len()]
+    'outer: for substr_len in 1..8 {
+        if len % substr_len != 0 || len <= substr_len {
+            repeats[substr_len-1] = false;
+            continue;
+        }
+
+        let substr = &str[0..substr_len];
+        for i in 1..len/substr_len {
+            if substr != &str[i*substr_len..(i+1)*substr_len] {
+                repeats[substr_len-1] = false;
+                continue 'outer;
+            }
+        }
+    }
+
+    repeats.contains(&true)
 }
 
 fn main() {
@@ -106,7 +39,7 @@ fn main() {
 
         for i in from..=to {
             if repeats_twice(i.to_string()) {password += i;}
-            if repeats(i) {password_2 += i;}
+            if repeats(i.to_string()) {password_2 += i;}
         }
 
     }
